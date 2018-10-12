f6db59b54f90b1cb8dc41e744df11339d0220c65
Namgyu Ho <itsnamgyu@gmail.com>
2018-10-12 10:41:57 +0900

    improvements to temp module main

--- cli/main.py
+++ cli/main.py
@@ -7,10 +7,14 @@ import re
 from collections import defaultdict
 import json
 
+import pandas as pd
+
 import lib
 from core import case
 from core import tester
 from core import compiler as cp
+
+n_problems = 4
 
 sources = glob.glob('source/*.c')
 id_re = re.compile('(20[0-9]{6})')
@@ -30,25 +34,30 @@ for _ in range(4):
     testsets.append(list())
 
 while True:
-    s = input('Add test case? (0, 1, 2, 3, q)')
+    s = input('Add test case? (1, 2, 3, 4, q) ')
     if s == 'q':
         break
     else:
         try:
-            testset = testsets[int(s)]
+            testset = testsets[int(s) - 1]
         except:
             continue
     
     string = lib.edit_string()
     testset.append(case.StdInputTest(string))
 
-    print('Test Cases'.center(100, '-'))
-    print(testsets)
+    for i, tests in enumerate(testsets):
+        print('Test Cases [{}]'.format(i + 1).center(100, '-'))
+        for j, test in enumerate(tests):
+            print('[{}] {}...'.format(j + 1, test.get_stdin().splitlines()[0]))
+    print('-' * 100)
 
 
-results = dict()
-for i in range(4):
-    for key, sources in students.items():
+results = pd.DataFrame(columns=list(range(n_problems)), index=students.keys())
+results.index.name = 'student_id'
+
+for i in range(n_problems):
+    for key, sources in sorted(students.items()):
         print('\n' * 5)
         print('{} - Program {}'.format(key, i).center(100, '*'))
         print()
@@ -72,5 +81,7 @@ for i in range(4):
             else:
                 print('Compile Error'.center(100, '-'))
                 print(cres['compile_error'])
-        ho = input('input results')
-        results['{}_{}'.format(key, i)] = ho
+        ho = input('Score: ')
+        results.loc[key, i] = ho
+
+results.to_csv('results.csv')
